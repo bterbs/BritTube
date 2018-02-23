2aed775e4f483f9aabb7053e81f9b476dc1c9ddf
B Terbush <brittanyterbush@gmail.com>
2018-02-23 15:54:16 -0800

    rename videos to video_list for better clarity and update index.js to use youtube data in app component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyDg_OPVPQWHkVN-J8_CjYCdCsNDaY9wWvg'
 
+
+
 //create a new component that should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'kittens'}, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 //tell React to render HTML in the DOM
