e1aa6f7ef25fef1fa948f043dba2150ae67e71f7
B Terbush <brittanyterbush@gmail.com>
2018-02-23 15:03:52 -0800

    Add youtube api key to index.js and turn searchbar into a class based component that records and reacts to user events via state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyDg_OPVPQWHkVN-J8_CjYCdCsNDaY9wWvg'
+
 //create a new component that should produce HTML
 const App = () => {
-  return <div>Hi there!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 //tell React to render HTML in the DOM
