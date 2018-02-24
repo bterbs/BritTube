ec5ca8666fc4c71934b12ea09fc3b6e5f3380630
B Terbush <brittanyterbush@gmail.com>
2018-02-23 16:26:56 -0800

    Call videolist component in app component

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: {} }
+    this.state = { videos: [] }
 
     YTSearch({key: API_KEY, term: 'kittens'}, (videos) => {
       this.setState({ videos })
