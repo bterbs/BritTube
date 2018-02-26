2869d16cc1e8c901296e356e2997cf0c5c1ec3f4
B Terbush <brittanyterbush@gmail.com>
2018-02-25 18:45:06 -0800

    Add function so that when user clicks on li, video plays in iframe

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-const VideoListItem = ({video}) => {
-  //const video = props.video
-  console.log(video);
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageURL = video.snippet.thumbnails.default.url
+
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video_list media">
         <div className="media-left">
           <img className="media-object" src={imageURL}/>
